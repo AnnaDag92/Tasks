2679e74d02ee9ab9b9ace9736a5fe7aabc506a6f
AnnaDag92 <78444190+AnnaDag92@users.noreply.github.com>
2021-06-16 18:34:50 +0400

    Add files via upload

---
+++ Find the key with iterators.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>​
+#include <string>​
+#include <vector>​
+
+struct Student
+{
+	int age; 
+	int avg;
+	std::string name;
+};
+
+std::vector <Student> vec;
+
+bool Cmp_Age(const Student& s, int x) {
+	return s.age == x;
+}
+bool Cmp_Avg(const Student& s, int x) {
+	return s.avg == x;
+}
+bool Cmp_Name(const Student& s, std::string x) {
+	return s.name == x;
+}
+
+template <typename Iter, typename Type, typename T>
+Iter find(Iter begin, Iter end, T key, bool(*cmp_ptr)(Type, T)) {
+	for (; Iter begin != Iter end; ++Iter) {
+		if (cmp_ptr(*begin, key))
+			return Iter;
+	}
+	return end;
+}
+int main()
+{
+	
+}
